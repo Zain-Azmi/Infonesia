d65a66b6e20772c8d12850923afdbd2ee89d7b9e
Zain Azmi <zainazmi50@gmail.com>
2025-05-27 14:13:11 +0700

    tambah gambar waktu hasil search kosong

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,16 @@ function App() {
             </div>
           ))
         ) : (
-          <p>Tidak Ada Negara Dengan Nama {InputSearch}</p>
+          <div className="flex flex-col items-center justify-center text-center mt-4">
+            <img
+              src="/bebek.gif"
+              alt="Bebek GIF"
+              className="w-32 h-auto mb-2"
+            />
+            <p className="text-gray-700">
+              Tidak ada negara dengan nama <strong>{InputSearch}</strong>
+            </p>
+          </div>
         )}
       </div>
       <br />
